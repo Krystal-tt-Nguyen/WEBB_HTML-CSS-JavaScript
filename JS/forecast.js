9bfc0c0882aa71480148b2318320d3300e1c3c2f
Krystal Nguyen <krystal.tt.nguyen@outlook.com>
2025-02-23 00:10:37 +0100

    Add product overview with product count, total price, and checkout button
    - Implemented a product overview section displaying the number of products and total price
    - Added functionality to update product quantities in the cart
    - Included a checkout button to proceed to the order completion page

--- JS/forecast.js
+++ JS/forecast.js
@@ -9,60 +9,58 @@ FetchWeatherInformation()
     }
 });
 
+
 async function FetchWeatherInformation()
 {
-    var url = 'https://api.open-meteo.com/v1/forecast?latitude=57.7072&longitude=11.9668&daily=temperature_2m_min,temperature_2m_max,apparent_temperature_max,apparent_temperature_min,weather_code&timezone=auto&forecast_days=7';            
+    const url = 'https://api.open-meteo.com/v1/forecast?latitude=57.7072&longitude=11.9668&daily=temperature_2m_min,temperature_2m_max,apparent_temperature_max,apparent_temperature_min,weather_code&timezone=auto&forecast_days=7';            
     try 
     {
-        var response = await fetch(url);
-        var data = await response.json();
+        let response = await fetch(url);
+        let data = await response.json();
         return data;
     }
     catch (error) 
     {
         console.log("Error fetching local weather: ", error);
+        return null;
     }
 }
 
-async function RenderWeatherInformation() {
-    var data = await FetchWeatherInformation(); 
-
-    if (data) {
-        var forecastContainer = document.getElementById('forecast');
+function RenderWeatherInformation(data) {
+    const forecastContainer = document.getElementById('forecast');
        
-        // map()-metoden skapar en ny array genom att anropa en funktion på varje element i den ursprungliga arrayen. 
-        // index är inbyggt funktion i JavaScript, hämtar det aktuella elementet i arrayen
-        var forecastList = data.daily.time.map((time, index) => {
-            return {
-                date: time,
-                minTemp: data.daily.temperature_2m_min[index],
-                maxTemp: data.daily.temperature_2m_max[index],
-                minAppTemp: data.daily.apparent_temperature_min[index],
-                maxAppTemp: data.daily.apparent_temperature_max[index],
-                weatherCode: data.daily.weather_code[index]
-            };
-        });
-        
-        forecastList.forEach(item => {
-            var forecast = document.createElement('div');
-            forecast.className = "mt-5 mb-3";
-            forecast.innerHTML = `
-                    <p>${item.date || "Unknown date"}</p>
-                    <p ><strong>Min:</strong> ${item.minTemp}°C (${item.minAppTemp}°C)</p>
-                    <p><strong>Max:</strong> ${item.maxTemp}°C (${item.maxAppTemp}°C)</p>
-                    <img src="${wmoCodeIcons[item.weatherCode]}" alt="forecast-icon-${wmoCodeText[item.weatherCode]}" width="80" height="80">
-                    <p>${wmoCodeText[item.weatherCode] || "Unknown weather description"}</p>
-                    `;
+    // map()-metoden skapar en ny array genom att anropa en funktion på varje element i den ursprungliga arrayen. 
+    // index är inbyggt funktion i JavaScript, hämtar det aktuella elementet i arrayen
+    var forecastList = data.daily.time.map((time, index) => {
+        return {
+            date: time,
+            minTemp: data.daily.temperature_2m_min[index],
+            maxTemp: data.daily.temperature_2m_max[index],
+            minAppTemp: data.daily.apparent_temperature_min[index],
+            maxAppTemp: data.daily.apparent_temperature_max[index],
+            weatherCode: data.daily.weather_code[index]
+        };
+    });
+    
+    forecastList.forEach(item => {
+        const forecast = document.createElement('div');
+        forecast.className = "mt-5 mb-3";
+        forecast.innerHTML = `
+                <p>${item.date || "Unknown date"}</p>
+                <p ><strong>Min:</strong> ${item.minTemp}°C (${item.minAppTemp}°C)</p>
+                <p><strong>Max:</strong> ${item.maxTemp}°C (${item.maxAppTemp}°C)</p>
+                <img src="${wmoCodeIcons[item.weatherCode]}" alt="forecast-icon-${wmoCodeText[item.weatherCode]}" width="80" height="80">
+                <p>${wmoCodeText[item.weatherCode] || "Unknown weather description"}</p>
+                `;
 
-                forecastContainer.appendChild(forecast)
-            }
-        );
-    }
+            forecastContainer.appendChild(forecast)
+        }
+    );
 }
 
 
 // JS Associative array --> Dictionary i C#
-var wmoCodeText = {
+const wmoCodeText = {
     0: "Clear sky",
     1: "Mainly clear",
     2: "Partly cloudy",
@@ -93,7 +91,7 @@ var wmoCodeText = {
     99: "Thunderstorm with heavy hail"
 };
 
-var wmoCodeIcons = {
+const wmoCodeIcons = {
     0: "https://openweathermap.org/img/wn/01d@2x.png", // Clear sky
     1: "https://openweathermap.org/img/wn/02d@2x.png", // Mainly clear
     2: "https://openweathermap.org/img/wn/03d@2x.png", // Partly cloudy
--- JS/products.js
+++ JS/products.js
@@ -1,5 +1,6 @@
-var productsArray = [
+const productsArray = [
     {
+        id: 1,
         name: "Original Semla",
         image: "/Images/product-semla-original.jpg",
         alt: "Original Swedish Semla with fluffy wheat bread, almond paste, and whipped cream",
@@ -10,6 +11,7 @@ var productsArray = [
         allergens: ["wheat", "almonds", "dairy"]
     },
     {
+        id: 2,
         name: "Vanilla Semla",
         image: "/Images/product-semla-vanilla.jpg",
         alt: "Vanilla Semla filled with smooth vanilla cream",
@@ -20,26 +22,29 @@ var productsArray = [
         allergens: ["wheat", "dairy"]
     },
     {
+        id: 3,
         name: "Almond Croissant",
         image: "/Images/product-croissant-almond.jpg",
         alt: "Almond Croissant filled with almond paste and topped with chopped almonds",
         description: "A buttery croissant filled with almond paste, topped with sweet glaze and chopped almonds.",
-        price: 59.00, 
+        price: 39.00, 
         currency: "kr",
         quantity: 0,
         allergens: ["wheat", "almonds", "dairy"]
     },
     {
+        id: 4,
         name: "Original Croissant",
         image: "/Images/product-croissant-original.jpg",
         alt: "Classic buttery croissant with a crispy exterior and flaky interior",
         description: "A classic buttery croissant with a crispy exterior and soft, flaky interior.",
-        price: 39.00, 
+        price: 25.00, 
         currency: "kr",
         quantity: 0,
         allergens: ["wheat", "dairy"]
     },
     {
+        id: 5,
         name: "Swedish Cinnamon Bun",
         image: "/Images/product-cinnamon-bun.jpg",
         alt: "Traditional Swedish cinnamon bun with a soft, sweet dough and a cinnamon-sugar filling",
@@ -50,6 +55,7 @@ var productsArray = [
         allergens: ["wheat", "dairy", "egg", "gluten"]
     },
     {
+        id: 6,
         name: "Swedish Kladdkaka",
         image: "/Images/product-kladdkaka.jpg",
         alt: "Sticky Swedish chocolate cake (Kladdkaka)",
@@ -60,6 +66,7 @@ var productsArray = [
         allergens: ["wheat", "dairy", "egg"]
     },
     {
+        id: 7,
         name: "Blackberry Cake",
         image: "/Images/product-blackberry-cake.jpg",
         alt: "Moist blackberry cake with creamy filling",
@@ -70,6 +77,7 @@ var productsArray = [
         allergens: ["wheat", "dairy", "egg"]
     },
     {
+        id: 8,
         name: "Carrot Cake",
         image: "/Images/product-carrot-cake.jpg",
         alt: "Carrot cake with cream cheese frosting and walnuts",
@@ -80,6 +88,7 @@ var productsArray = [
         allergens: ["wheat", "dairy", "egg", "nuts"]
     },
     {
+        id: 9,
         name: "Chocolate Cake",
         image: "/Images/product-chocolate-cake.jpg",
         alt: "Decadent chocolate cake with layers of ganache",
@@ -90,6 +99,7 @@ var productsArray = [
         allergens: ["wheat", "dairy", "egg"]
     },
     {
+        id: 10,
         name: "Strawberry Cake",
         image: "/Images/product-strawberry-cake.jpg",
         alt: "Light strawberry cake with whipped cream topping",
@@ -100,6 +110,7 @@ var productsArray = [
         allergens: ["wheat", "dairy"]
     },
     {
+        id: 11,
         name: "Blueberry Cheesecake",
         image: "/Images/product-blueberry-cheesecake.jpg",
         alt: "Blueberry Cheesecake with fresh blueberries",
@@ -110,6 +121,7 @@ var productsArray = [
         allergens: ["wheat", "dairy", "egg"]
     },
     {
+        id: 12,
         name: "New York Cheesecake",
         image: "/Images/product-newyork-cheesecake.jpg",
         alt: "Rich New York-style cheesecake with graham cracker crust",
@@ -120,6 +132,7 @@ var productsArray = [
         allergens: ["wheat", "dairy", "egg"]
     },
     {
+        id: 13,
         name: "Lemon Pie",
         image: "/Images/product-lemon-pie.jpg",
         alt: "Tangy lemon pie with buttery, flaky crust",
@@ -132,28 +145,28 @@ var productsArray = [
 ];
 
 /* ----- Products.html ----- */
-var productsContainer = document.getElementById('products-container'); 
+const productsContainer = document.getElementById('products-container'); 
 
 productsArray.forEach(function(product, index) {
-    var productElement = LoadProduct(product, index);
+    let productElement = LoadProduct(product, index);
     productsContainer.appendChild(productElement);
 });
 
 function LoadProduct(product, index) {
-    var productDiv = document.createElement('div');
+    const productDiv = document.createElement('div');
     productDiv.className = "col-11 col-sm-6 col-md-4 col-lg-3 mt-3 mb-5 product-div";
 
-    var productImageContainer = document.createElement('div');
+    const productImageContainer = document.createElement('div');
     productImageContainer.className = "product-image-container";
 
-    var productImage = document.createElement('img');
+    const productImage = document.createElement('img');
     productImage.src = product.image;
     productImage.alt = product.alt || `product-image-${index}`;
     productImage.className = "product-image";
     productImage.height = 350;
     productImage.width = 300;
 
-    var viewDetailsButton = document.createElement('button');
+    const viewDetailsButton = document.createElement('button');
     viewDetailsButton.className = 'btn btn-primary view-details-btn';
     viewDetailsButton.textContent = 'View Details';
     viewDetailsButton.setAttribute('data-bs-toggle', 'modal');
@@ -166,10 +179,10 @@ function LoadProduct(product, index) {
     productImageContainer.appendChild(productImage);
     productImageContainer.appendChild(viewDetailsButton);
 
-    var productName = document.createElement('h5');
+    const productName = document.createElement('h5');
     productName.innerText = product.name || `product-name-${index}`;
 
-    var productPrice = document.createElement('p');
+    const productPrice = document.createElement('p');
     productPrice.innerHTML = `<strong>Price</strong> ${product.price || 'N/A'} ${product.currency}`;
 
     productDiv.appendChild(productImageContainer);
@@ -181,12 +194,12 @@ function LoadProduct(product, index) {
 
 
 /* ----- Products Modal ----- */
-var modalHeader = document.querySelector('.modal-header');
-var modalBody = document.querySelector('.modal-body');
-var modalFooter = document.querySelector('.modal-footer');
+const modalHeader = document.querySelector('.modal-header');
+const modalBody = document.querySelector('.modal-body');
+const modalFooter = document.querySelector('.modal-footer');
 
 function ShowProductModal(product, index) {
-    var allergensList = product.allergens.join(", ");
+    let allergensList = product.allergens.join(", ");
     
     modalHeader.innerHTML = `
         <h5 class="mt-3">${product.name}</h5>
@@ -207,71 +220,128 @@ function ShowProductModal(product, index) {
     `;
 
     modalFooter.innerHTML = "";
-    var options = LoadPurchaseOptions(product);
+    var options = LoadPurchaseOption(product);
     modalFooter.appendChild(options);
 }
 
-
-/* ----- Products Purchase Options ----- */
-function LoadPurchaseOptions(product) {
-    var purchaseDiv = document.createElement('div');
+function LoadPurchaseOption(product) {
+    const purchaseDiv = document.createElement('div');
     purchaseDiv.className = 'purchase-options';
-
+    
     purchaseDiv.innerHTML = `
-        <button class="product-button-style cart-button" aria-label="Add to cart">
-            <i class="fa fa-cart-plus"></i>
+        <button class="cart-button" aria-label="Add to cart">
+        <i class="fa fa-cart-plus"></i>
         </button>
     `;
 
-    var cartButton = purchaseDiv.querySelector('.cart-button');
-
-    cartButton.addEventListener('click', function() {
-        // Skriva kod för att lägga till produkten i kundvagnen
-    });
+    AddToCart(purchaseDiv, product);
 
     return purchaseDiv;
 }
 
 
+/* ----- Shopping Cart ----- */
+const cartBody = document.getElementById('cart-body');
+const cartFooter = document.getElementById('cart-footer');
+const navbarDropdown = document.getElementById('navbarDropdown');
 
+const cartItemsArray = [];
 
-// purchaseDiv.innerHTML = `
-//         <button class="product-button-style minus-button" aria-label="Decrease product quantity">-</button>
+function AddToCart(purchaseDiv, product) {
+    const cartButton = purchaseDiv.querySelector('.cart-button');
+    cartButton.addEventListener('click', function() {
 
-//         <input type="number" class="quantity-display" aria-label="Display" value="${product.quantity}">
+        //find() i JavaScript motsvarar ungefär FirstOrDefault i C#
+        const existingProduct = cartItemsArray.find(item => item.id == product.id);
 
-//         <button class="product-button-style plus-button" aria-label="Increase product quantity">+</button>
-//     `;
+        if(existingProduct)
+        {
+            existingProduct.quantity++;
+        }
+        else
+        {
+            // Spread operator (...) möjliggör kopiering av hela eller delar av en befintlig array eller objekt till en annan
+            // => ändringar som görs i kundvagnen påverkar ej quantity i productsArray
+            const productCopy = {...product};
+            productCopy.quantity = 1;
+            cartItemsArray.push(productCopy);
+        }
+        
+        UpdateShoppingCart();
+        
+    });
+}
 
-//     var minusButton = purchaseDiv.querySelector('.minus-button');
-//     var quantityDisplay = purchaseDiv.querySelector('.quantity-display');
-//     var plusButton = purchaseDiv.querySelector('.plus-button');
-   
-//     minusButton.addEventListener('click', function() {
-//         if (product.quantity > 0)
-//         {
-//             product.quantity--;
-//             quantityDisplay.value = product.quantity;
-//         }
-//     });
+function UpdateShoppingCart() {
+    cartBody.innerHTML = '';
+    let totalPrice = 0;
 
-//     quantityDisplay.addEventListener('input', function() {
-//         var newQuantity = parseInt(quantityDisplay.value);
+    cartItemsArray.forEach(cartItem => {
+        let cartElement = LoadCartItem(cartItem);
+        cartBody.appendChild(cartElement);
+        totalPrice += (cartItem.quantity * cartItem.price);
+        
+        console.log(`${cartItem.name} ${cartItem.quantity}`);
+    });
+    
+    cartFooter.innerHTML = `
+        <p><strong>Total price:</strong> ${totalPrice} ${productsArray[0].currency}</p>
+        <button id="check-out">Check Out</button>
+    `;
 
-//         if (!isNaN(newQuantity) && newQuantity >= 0 && newQuantity <= 100) 
-//         {
-//             product.quantity = newQuantity;
-//         }
-//         else
-//         {
-//             quantityDisplay.value = product.quantity;
-//         }
-//     })
+    if (cartItemsArray.length > 0) {
+        navbarDropdown.innerHTML = `
+            <i id="check-out-logo" class="fa-solid fa-cart-shopping fa-beat"></i> (${cartItemsArray.length})
+        `;
+    }
+    else {
+        navbarDropdown.innerHTML = `<i id="check-out-logo" class="fa-solid fa-cart-shopping"></i>`;
+    }
+}
 
-//     plusButton.addEventListener('click', function() {
-//         if (product.quantity < 100) 
-//         {
-//             product.quantity++;
-//             quantityDisplay.value = product.quantity;
-//         }
-//     });
+function LoadCartItem(cartItem) {
+    const shoppingCartDiv = document.createElement('div');
+    shoppingCartDiv.className = "shopping-cart-div mb-3";
+    let subTotal = cartItem.price * cartItem.quantity;
+
+    if(cartItem.quantity > 0)
+    {
+        shoppingCartDiv.innerHTML = `
+            <div id="cart-image-container">
+                <img src="${cartItem.image}" alt="${cartItem.alt}" class="rounded-2" width="100", height="100">
+            </div>
+            <div class="cart-item-details text-start">
+                <p><strong>${cartItem.name}</strong></p>
+                <p><i>${cartItem.price} ${cartItem.currency}</i></p>
+            </div>
+            <input type="number" class="quantity-display mx-auto" min="0" max="100" aria-label="Display" value="${cartItem.quantity}">
+            <p class="mx-auto"><strong>Subtotal:</strong> ${subTotal} ${cartItem.currency}</p>
+        `;
+    }
+        
+    const quantityDisplay = shoppingCartDiv.querySelector('.quantity-display');
+    
+    quantityDisplay.addEventListener('input', function() {
+        var newQuantity = parseInt(quantityDisplay.value);
+
+        if (!isNaN(newQuantity) && newQuantity >= 0 && newQuantity <= 100) 
+        {
+            cartItem.quantity = newQuantity;
+        }
+        
+        // Javascript har ej array.Remove() --> för ta bort ett specifikt objekt används array.slice(start, end)
+        if (newQuantity == 0) 
+        {
+            // findIndex() returnerar index för första elementet som matchar ett villkor. Returnerar -1 om inget matchar
+            const index = cartItemsArray.findIndex(i => i.id == cartItem.id);
+            if (index != -1) {
+                cartItemsArray.splice(index, 1);
+            }
+        }
+
+        UpdateShoppingCart();
+    })
+
+    return shoppingCartDiv;
+}
+
