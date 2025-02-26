af28ea5c7c79117d6736908a87d59c8e1867c3d1
Krystal Nguyen <krystal.tt.nguyen@outlook.com>
2025-02-26 20:13:47 +0100

    (WIP) Refactored navigation: Moved nav from separate file to each HTML file and updated file paths

--- JS/script.js
+++ JS/script.js
@@ -1,22 +1,13 @@
-fetch('/HTML/header.html')
-    .then(response => response.text())
-    .then(data => {
-        document.getElementById('header').innerHTML = data;
-    })
-    .catch(error => {
-        console.error('Unable to load Header: ', error)
+fetch('../HTML/header.html')
+.then(response => response.text())
+.then(data => {
+    document.getElementById('header').innerHTML = data;
+})
+.catch(error => {
+    console.error('Unable to load Header: ', error)
 });
 
-fetch('/HTML/nav.html')
-    .then(response => response.text())
-    .then(data => {
-        document.getElementById('navbar').innerHTML = data
-    })
-    .catch(error => {
-        console.error('Unable to load Navbar: ', error)
-});
-
-fetch('/HTML/footer.html')
+fetch('../HTML/footer.html')
     .then(response => response.text())
     .then(data => {
         document.getElementById('footer').innerHTML = data
